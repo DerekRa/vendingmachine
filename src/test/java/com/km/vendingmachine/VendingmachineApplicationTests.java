826e1726132b706f4bd5010a8bb3a10b9cf0b2d5
Kenneth M <armstrong17.km@gmail.com>
2021-09-03 16:59:46 +0800

    add initial spring boot app

---
+++ src/test/java/com/km/vendingmachine/VendingmachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.km.vendingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingmachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
